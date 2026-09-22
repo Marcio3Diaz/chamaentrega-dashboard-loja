5bea7018aa7b523bfbf56d2f2d3483e41427c20c
Marcio Diaz <marcio3diaz@gmail.com>
2026-09-22 03:36:04 -0300

    Usa URL pública validada no metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from 'next'
 import Script from 'next/script'
+import { getSiteUrl } from '@/lib/site-url'
 
-const siteUrl = process.env.NEXT_PUBLIC_SITE_URL?.replace(/\/$/, '') || 'http://localhost:3000'
+const siteUrl = getSiteUrl()
 
 export const metadata: Metadata = {
   metadataBase: new URL(siteUrl),
