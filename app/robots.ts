47506ff0c2971ab64b1b6e64c5d09a00ff2e3097
Marcio Diaz <marcio3diaz@gmail.com>
2026-09-21 22:10:49 -0300

    Adiciona robots.txt para SEO

---
+++ app/robots.ts
@@ -0,0 +1,35 @@
+import type { MetadataRoute } from 'next'
+
+const siteUrl =
+  process.env.NEXT_PUBLIC_SITE_URL?.replace(/\/$/, '') ||
+  'http://localhost:3000'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: [
+          '/admin',
+          '/painel',
+          '/chat',
+          '/configuracoes',
+          '/despacho',
+          '/entregadores',
+          '/entregas',
+          '/financeiro',
+          '/integracoes',
+          '/mapa',
+          '/pedidos',
+          '/onboarding',
+          '/login',
+          '/forgot-password',
+          '/reset-password',
+        ],
+      },
+    ],
+    sitemap: `${siteUrl}/sitemap.xml`,
+    host: siteUrl,
+  }
+}
